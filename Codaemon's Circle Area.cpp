5959dacb50590241248ed42e79e7f286f63effb3
Code4All <75253091+code4allrgpv@users.noreply.github.com>
2021-12-06 21:15:53 +0530

    Create Codaemon's Circle Area.cpp

---
+++ Codaemon's Circle Area.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<fixed<<setprecision(2)<<n*n*3.14<<endl;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
